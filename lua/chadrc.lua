8562cd2b951f8a8652962f51eee19c3bb89b6d64
Roshantigga <99roshantigga@gmail.com>
2025-02-09 14:08:53 +0530

    Updated chadrc.lua with tab management keybindings

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,14 +4,17 @@ local M = {}
 local overrides = require("configs.overrides")
 
 M.ui = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    theme = "onedark",
 }
 
-M.ui = overrides.ui
+-- Merge overrides without replacing the theme
+M.ui = vim.tbl_deep_extend("force", M.ui, overrides.ui or {})
+
+-- Key mappings for tab management
+vim.api.nvim_set_keymap("n", "<leader>tn", ":tabnew<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tl", "gt", { noremap = true, silent = true }) -- Next tab
+vim.api.nvim_set_keymap("n", "<leader>th", "gT", { noremap = true, silent = true }) -- Previous tab
 
 return M
+
